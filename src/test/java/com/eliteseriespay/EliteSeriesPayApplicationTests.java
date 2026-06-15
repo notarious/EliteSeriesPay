4cf3f5c8a0669afd3e43055847ea024204b5e7b1
Alexey Notarius <alexeynotarius@gmail.com>
2026-06-16 00:25:11 +0300

    Add Spring Boot project skeleton

---
+++ src/test/java/com/eliteseriespay/EliteSeriesPayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eliteseriespay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteSeriesPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
